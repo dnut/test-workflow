8765239da4b518582ea4159fa944c4ff984126b8
dnut <dnut@users.noreply.github.com>
2026-01-06 21:57:56 -0300

    reorganize and add libclang-dev

--- generate.sh
+++ generate.sh
@@ -1,34 +1,26 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-NEED_UBUNTU_DEPS=${NEED_UBUNTU_DEPS:-true}
+INSTALL_SYSTEM_DEPS=${INSTALL_SYSTEM_DEPS:-ubuntu}
 
 cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Install system dependencies
-if [[ $NEED_UBUNTU_DEPS == true ]]; then
+if [[ $INSTALL_SYSTEM_DEPS == ubuntu ]]; then
     sudo add-apt-repository ppa:deadsnakes/ppa -y
     sudo apt-get update
     sudo apt install -y python3.11 python3.11-dev python3.11-venv rename zstd \
-        libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang cmake make libprotobuf-dev protobuf-compiler
+        libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang cmake make libprotobuf-dev protobuf-compiler libclang-dev
+elif [[ $INSTALL_SYSTEM_DEPS != '' ]]; then
+    echo unknown system, not installing dependencies: $INSTALL_SYSTEM_DEPS
 fi
 
-# Clone sig repo
-if [[ ! -d "sig" ]]; then
-    git clone --depth=1 https://github.com/Syndica/sig.git sig
-fi
-
+git clone --depth=1 https://github.com/Syndica/sig.git sig
 pushd sig/conformance
-
-# Install conformance code
-yes | scripts/setup-env.sh
-
-# Activate the Python environment
-source env/pyvenv/bin/activate
-
-# Create fixtures without running them
-./run.py --create --no-run
+yes | scripts/setup-env.sh       # Install conformance code
+source env/pyvenv/bin/activate   # Activate the Python environment
+./run.py --create --no-run       # Create test fixtures
+popd
 
 # Create tarball of generated fixtures
-popd
 tar --zstd -cf fixtures.tar.zst -C sig/conformance/env test-fixtures

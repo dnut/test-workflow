507d0eb60de69ae1a8f91adbf301b90dda424afd
dnut <dnut@users.noreply.github.com>
2026-01-06 21:19:24 -0300

    initial commit

---
+++ generate.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+NEED_UBUNTU_DEPS=${NEED_UBUNTU_DEPS:-true}
+
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Install system dependencies
+if [[ $NEED_UBUNTU_DEPS == true ]]; then
+    sudo add-apt-repository ppa:deadsnakes/ppa -y
+    sudo apt-get update
+    sudo apt install -y python3.11 python3.11-dev python3.11-venv rename zstd
+fi
+
+# Clone sig repo
+if [[ ! -d "sig" ]]; then
+    git clone --depth=1 https://github.com/Syndica/sig.git sig
+fi
+
+pushd sig/conformance
+
+# Install conformance code
+yes | scripts/setup-env.sh
+
+# Activate the Python environment
+source env/pyvenv/bin/activate
+
+# Create fixtures without running them
+./run.py --create --no-run
+
+# Create tarball of generated fixtures
+popd
+tar --zstd -cf fixtures.tar.zst -C sig/conformance/env test-fixtures
